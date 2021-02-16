8c642bd745db034f1ba5a3468d429021a87c21b2
TKA Robotics <TKA.robotics@gmail.com>
2021-02-16 09:40:55 -0500

    last commit added connection string for previous project, fixed

--- Startup.cs
+++ Startup.cs
@@ -31,11 +31,8 @@ namespace DotNetCoreSqlDb
         {
             services.AddControllersWithViews();
 
-            // services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer("Server=tcp:robotdbtestserver.database.windows.net,1433;Database=coreDB;User ID=jatler;Password=TKA_R0bot1c$_db;Encrypt=true;Connection Timeout=30;"));
-            // new password: TKA_R0bot1c$_db
-
             services.AddDbContext<MyDatabaseContext>(options =>
-            options.UseSqlServer("Server=tcp:robotdbtestserver.database.windows.net,1433;Database=coreDB;User ID=jatler;Password=TKA_R0bot1c$_db;Encrypt=true;Connection Timeout=30"));
+            options.UseSqlServer("Server=tcp:tkainventoryserver.database.windows.net,1433;Database=coreDB;User ID=tka;Password=M1dKnightRobotic$;Encrypt=true;Connection Timeout=30;"));
 
             services.AddIdentity<IdentityUser, IdentityRole>(config =>
             {
